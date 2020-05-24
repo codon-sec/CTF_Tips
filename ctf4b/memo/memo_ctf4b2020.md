ef9009ed36f66328432edf32348e8d17dcd07766
yoshio36 <yoshiooyama35@gmail.com>
2020-05-25 00:38:31 +0900

    add

--- ctf4b/memo/memo_ctf4b2020.md
+++ ctf4b/memo/memo_ctf4b2020.md
@@ -5,6 +5,7 @@
 ## Misc
 ### emoemoencode
     U+1F300-U+1F3FFまでが絵文字であるためUnicordの下8bitにASCII文字列を対応させている。
+    U+1F363 → c (63) ascii
     絵文字から戻すときはUnicode表示(pythonのordもOK)にしてから0x1F300を引く
 
 ### readme
@@ -18,9 +19,22 @@
 ### spy
     ログイン時の実行時間の差で利用ユーザを判断することができる。
 
+
+### tweetstore
+    配布コードを読んでいくとsql文を
+    select url, text, tweeted_at from tweets where text like '%{search}%' order by tweet_at desc limit
+    条件: searchは'が使えない。
+    このlimitの後がインジェクション対策を怠っており攻撃が可能になっている。がsplitされるためクエリを投げられない。
+
+
+### unzip
+
+
 ## pwn
     まだ練習不足であり、本CTFでは取り組まなかった。
+    pythonのpwntoolを使用している人が多い印象。
 
 ## rev
     上に同じく、チームメンバーに任せたが次回は取り組みたい。
+    angrを使用すると簡単に解けるらしい。
 
--- ctf4b/r_and_b/rot13_base.py
+++ ctf4b/r_and_b/rot13_base.py
@@ -16,10 +16,21 @@ with open('encoded_flag', 'r') as f:
 def rot13(s):
     return alphabet[(alphabet.index(s) + 13) % 26]
 
+def rot13_str(cipher):
+    plain = ''
+    for c in cipher:
+        if c.isupper():
+            plain += rot13(c.lower()).upper()
+        elif c.islower():
+            plain += rot13(c)
+        else:
+            plain += c
+    return plain
+
 print(encoded_flag)
 while True:
     if encoded_flag[0] == 'R':
-        encoded_flag = rot13(encoded_flag[1:])
+        encoded_flag = rot13_str(encoded_flag[1:])
         print(encoded_flag)
     elif encoded_flag[0] == 'B':
         encoded_flag = base64.b64decode(encoded_flag[1:]).decode()
