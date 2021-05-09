ced04ca74b70181e279c7754b34ae06920623680
codon-sec <yoshiooyama36@gmail.com>
2021-05-10 07:36:16 +0900

    add icats.sh

---
+++ ksnctf/icats.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+icat drive.img 36-128-4 >> flag.txt
+icat drive.img 36-128-5 >> flag.txt
+icat drive.img 36-128-6 >> flag.txt
+icat drive.img 36-128-7 >> flag.txt
+icat drive.img 36-128-8 >> flag.txt
+icat drive.img 36-128-9 >> flag.txt
+icat drive.img 36-128-10 >> flag.txt
