bc3b50997c417c21df384c2fa8c116433e6775f7
codon-sec <yoshiooyama36@gmail.com>
2021-09-20 12:16:21 +0900

    add strict-dynamic.py

---
+++ web/app/bypass_strict-dynamic.py
@@ -0,0 +1,38 @@
+# DOM Clobberingによるstrict-dynamicのバイパス
+import base64
+import random
+
+from flask import Flask, Response, request
+
+app = Flask(__name__)
+
+
+def get_nonce():
+    return f'{base64.b64encode(str(random.random()).encode()).decode()}'
+
+
+@app.route('/')
+def index():
+    name = request.args.get('name', 'unknown')
+    title = request.args.get('title', 'unknown')
+
+    nonce = get_nonce()
+
+    res = Response(f'''
+    <script nonce="{nonce}">
+        windows.onload = () => {{
+            const name = (new URL(location).searchParams).get('name')
+            document.getElementById("greeting").innerHTML = `${{name ?? 'unknown'}}!`
+        }}
+    </script>
+    <h1>{title}</h1>
+    <p id="greeting">Hello, {name}!</p>
+    ''')
+
+    res.headers[
+        'Content-Security-Policy'] = f'default-src \'none\'; script-src \'strict-dynamic\' \'nonce-{nonce}\''
+
+    return res
+
+
+app.run(host='0.0.0.0', port=5000)
